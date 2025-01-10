3ee58aecbcc8c5f177a4f67c70e9373575997f86
Nur Mahammad Mondol Robiul <nurmahammadmondolrobiul@gmail.com>
2025-01-09 22:32:51 -0400

    Added put, delete method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
@@ -24,22 +24,92 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const database = client.db('Restaurant_Management');
-    const AllFoods = database.collection('All_Foods');
+    const AllFoods = client.db('Restaurant_Management').collection('All_Foods');
+    const OrderFoods = client
+      .db('Restaurant_Management')
+      .collection('Order_Foods');
 
     app.get('/AllFoods', async (req, res) => {
-      const All_Food = AllFoods.find();
+      const CurrentPage = parseInt(req.query.CurrentPage);
+      const parPageItem = parseInt(req.query.parPageItem);
+
+      const All_Food = AllFoods.find()
+        .skip(CurrentPage * parPageItem - parPageItem)
+        .limit(parPageItem);
       const result = await All_Food.toArray();
+      res.send(result);
+    });
+
+    app.get('/AllFoodsCount', async (req, res) => {
+      const count = await AllFoods.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    app.get('/AllFoods/:id', async (req, res) => {
+      const ID = req.params.id;
+      const RealID = { _id: new ObjectId(ID) };
+      const result = await AllFoods.findOne(RealID);
       res.send(result);
     });
 
     // Add Data client site & push database.
     app.post('/AllFoods', async (req, res) => {
       const Data = req.body;
-      console.log('server data add', Data);
+      // console.log('server data add', Data);
       const result = await AllFoods.insertOne(Data);
       res.send(result);
     });
+
+    app.put('/AllFoods/:id', async (req, res) => {
+      const filterID = req.params.id;
+      const RealID = { _id: new ObjectId(filterID) };
+      const Food = req.body;
+
+      // FoodName, Price, Quantity, FoodImage, FoodCategory, Description;
+
+      const updateProduct = {
+        $set: {
+          FoodName: Food.FoodName,
+          FoodCategory: Food.FoodCategory,
+          Price: Food.Price,
+          FoodImage: Food.FoodImage,
+          Quantity: Food.Quantity,
+          Description: Food.Description,
+        },
+      };
+
+      const result = await AllFoods.updateOne(RealID, updateProduct);
+      res.send(result);
+    });
+
+    app.post('/OrderFoods', async (req, res) => {
+      const Data = req.body;
+      // console.log('server order data ', Data);
+      const result = await OrderFoods.insertOne(Data);
+      res.send(result);
+    });
+
+    app.get('/OrderFoods', async (req, res) => {
+      const Order_Foods = OrderFoods.find();
+      const result = await Order_Foods.toArray();
+      res.send(result);
+    });
+
+    app.get('/OrderFoods/:id', async (req, res) => {
+      const ID = req.params.id;
+      const RealID = { _id: new ObjectId(ID) };
+      const result = await OrderFoods.findOne(RealID);
+      res.send(result);
+    });
+
+    app.delete('/OrderFoods/:id', async (req, res) => {
+      const RealID = req.params.id;
+      const FilterID = { _id: new ObjectId(RealID) };
+      const result = await OrderFoods.deleteOne(FilterID);
+      res.send(result);
+    });
+
+    // app.delete('/OrderFoods');
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
