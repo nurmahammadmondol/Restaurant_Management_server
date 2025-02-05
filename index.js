cc017770e547eff3204a05af7767d45a462611ee
Nur Mahammad Mondol Robiul <nurmahammadmondolrobiul@gmail.com>
2025-02-05 11:52:27 -0400

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const AllFoods = client.db('Restaurant_Management').collection('All_Foods');
     const OrderFoods = client
